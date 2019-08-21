9abba9979a18bc7a5b7297ca49b5359eee71e0a9
Παλαιολόγος <kspalaiologos@gmail.com>
2019-08-21 10:01:57 +0200

    Create c0.c

---
+++ c0.c
@@ -0,0 +1,3 @@
+#include<stdio.h>
+#define c(x)case x:while((c=getchar())!=-1)
+char*x="+b(29e*j1VMEKLyC})8&m#~W>qxdRp0wkrUo[D7,XTcA\"lI.v%{gJh4G\\-=O@5`_3i<?Z';FNQuY]szf$!BS/|t:Pn6^Ha",c,i,j;main(c,v)char**v;{if(c==2)switch(v[1][1]){c('e'){for(j=0;j<94;j++){if(c==x[(j+i)%94]){c=j+33;break;}}putchar(c);i++;}break;c('d')putchar(x[(c-33+i++)%94]);break;}}
