089ee3f7f934f312861e8376fe2e950615307d7a
Παλαιολόγος <kspalaiologos@gmail.com>
2019-09-04 18:31:28 +0200

    Create block-sandbox-infobox.js

---
+++ block-sandbox-infobox.js
@@ -0,0 +1,7 @@
+// ==UserScript==
+// @name         Hide the sandbox infobox.
+// @version      0.1
+// @match        https://codegolf.stackexchange.com/
+// ==/UserScript==
+
+(function(){'use strict';document.querySelector('.s-sidebarwidget').hidden=true;})();
